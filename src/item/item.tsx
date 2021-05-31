faddfc1ac143b570fdb15b1956a8380a339a03a1
ankit <69717009+crew-guy@users.noreply.github.com>
2021-05-31 18:30:06 +0530

    setup item interface

--- src/item/item.tsx
+++ src/item/item.tsx
@@ -1,4 +1,12 @@
 import {Button} from '@material-ui/core'
 
 // Types
-import {CartItemType} from '../App'
+import { CartItemType } from '../App'
+
+// Styles
+import {Wrapper} from './item.styles'
+
+type Props = {
+    item: CartItemType;
+    handleAddToCart
+}
